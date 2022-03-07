9258e118598270c2a06d807dd1e9f5fa58e4bd72
Mercy Rose Santos <mercy.santos@pointwest.com.ph>
2022-03-07 15:15:56 +0800

    add Application

---
+++ Backend/src/main/java/com/company/pastebook/Pastebook.java
@@ -0,0 +1,13 @@
+package com.company.pastebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pastebook {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pastebook.class, args);
+	}
+
+}
