2dc36ee9becd85caebe4a5113621666dd4eee467
mercyrosesantos <98379398+mercyrosesantos@users.noreply.github.com>
2022-03-07 18:23:19 +0800

    model branch (#1)

--- Backend/src/test/java/com/company/discussion/PastebookApplicationTests.java
+++ Backend/src/test/java/com/company/discussion/PastebookApplicationTests.java
@@ -1,10 +1,10 @@
-package com.company.pastebook;
+package com.company.discussion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscussionApplicationTests {
+class PastebookApplicationTests {
 
 	@Test
 	void contextLoads() {
