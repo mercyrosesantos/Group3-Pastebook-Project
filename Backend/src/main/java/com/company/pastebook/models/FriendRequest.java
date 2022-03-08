621849a7fbd64ad7b25c45a309238b553bddbbcc
Stephanie Landingin <stephanie.landingin@pointwest.com.ph>
2022-03-08 09:54:20 +0800

    Added friend request model

---
+++ Backend/src/main/java/com/company/pastebook/models/FriendRequest.java
@@ -0,0 +1,73 @@
+package com.company.pastebook.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "friendRequests")
+public class FriendRequest {
+
+    // Properties
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "friendRequests_seq")
+    @SequenceGenerator(name = "friendRequests_seq", sequenceName = "sequence_friendRequests", allocationSize = 1)
+    private Long id;
+
+    @Column
+    private String requestTimestamp;
+
+    @Column
+    private String status = "pending";
+
+    // Requestor Id
+    @Column
+    private Long requestorId;
+
+    // Requestee Id
+    @ManyToOne
+    @JoinColumn (name = "requesteeId", nullable = true)
+    private User requestee;
+
+    // Constructor
+
+    public FriendRequest() {
+    }
+
+    // Getter and Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRequestTimestamp() {
+        return requestTimestamp;
+    }
+
+    public void setRequestTimestamp(String requestTimestamp) {
+        this.requestTimestamp = requestTimestamp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getRequestorId() {
+        return requestorId;
+    }
+
+    public void setRequestorId(Long requestorId) {
+        this.requestorId = requestorId;
+    }
+
+    public User getRequestee() {
+        return requestee;
+    }
+
+    public void setRequestee(User requestee) {
+        this.requestee = requestee;
+    }
+}
