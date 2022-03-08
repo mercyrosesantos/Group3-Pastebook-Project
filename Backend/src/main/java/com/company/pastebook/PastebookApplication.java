7959e0e5a071b373678df4f41a51078fd0876288
Aldriannnnn <aldrian.blanquera@pointwest.com.ph>
2022-03-08 09:25:13 +0800

    Updated Registration

---
+++ Backend/src/main/java/com/company/pastebook/PastebookApplication.java
@@ -0,0 +1,13 @@
+package com.company.pastebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastebookApplication.class, args);
+	}
+
+}
