8a787a5eb78fa79efd502fde2abe5b1ff0224f8d
Mercy Rose Santos <mercy.santos@pointwest.com.ph>
2022-03-09 21:44:22 +0800

    updates on verification email

--- Backend/src/main/java/com/company/pastebook/services/UserServiceImp.java
+++ Backend/src/main/java/com/company/pastebook/services/UserServiceImp.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
 import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.Properties;
 
 @Service
 public class UserServiceImp implements UserService {
@@ -25,7 +30,7 @@ public class UserServiceImp implements UserService {
     private User user;
 
     @Autowired
-    private JavaMailSender javaMailSender;
+    private JavaMailSenderImpl mailSenderImpl;
 
 //    Register User
     public void createUser(User user, String siteUrl) {
@@ -62,7 +67,7 @@ public class UserServiceImp implements UserService {
 //    Sending Verification Email
     public void sendVerificationEmail(User user, String siteUrl) throws MessagingException, UnsupportedEncodingException {
         String toAddress = user.getEmail();
-        String fromAddress = "aldrianblanquera000@gmail.com";
+        String fromAddress = "pastebook.group3@gmail.com";
         String senderName = "Pastebook Development Team";
         String subject = "Please verify your registration";
         String content = "Dear [[name]],<br>"
@@ -71,7 +76,7 @@ public class UserServiceImp implements UserService {
                 + "Thank you,<br>"
                 + "Pastebook Development Team.";
 
-        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessage message = mailSenderImpl.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message);
 
         helper.setFrom(fromAddress, senderName);
@@ -82,8 +87,11 @@ public class UserServiceImp implements UserService {
         String verifyURL = siteUrl + "/verify?code=" + user.getVerificationCode();
         content = content.replace("[[URL]]", verifyURL);
         helper.setText(content, true);
-        javaMailSender.send(message);
+        mailSenderImpl.setUsername("pastebook.group3@gmail.com");
+        mailSenderImpl.setPassword("Pastebook1!");
+        mailSenderImpl.send(message);
     }
+
 
 
 }
