f95fa104f90334f3c56a8668f92462662f633ac1
Aldriannnnn <aldrian.blanquera@pointwest.com.ph>
2022-03-09 18:09:14 +0800

    First Send Email Verification Update

--- Backend/src/main/java/com/company/pastebook/services/UserService.java
+++ Backend/src/main/java/com/company/pastebook/services/UserService.java
@@ -3,11 +3,13 @@ package com.company.pastebook.services;
 import com.company.pastebook.models.User;
 import org.springframework.http.ResponseEntity;
 
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import java.util.Optional;
 
 public interface UserService {
 //    Register user
-    void createUser(User user);
+    void createUser(User user, String siteUrl);
 
 //    Find by email
     Optional<User> findByEmail(String email);
@@ -18,6 +20,6 @@ public interface UserService {
 // Get User Profile
     ResponseEntity getUserProfile(Long id);
 
-
-
+//    Send Verification Email
+    void sendVerificationEmail(User user, String siteUrl) throws MessagingException, UnsupportedEncodingException;
 }
